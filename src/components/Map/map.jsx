8f2a63825a4e6b475919907fe38347ce64133f15
MagdalenaKuch <57178283+MagdalenaKuch@users.noreply.github.com>
2024-06-26 16:03:00 +0200

    E1-Add-geojson-use-point-helper

--- src/components/Map/map.jsx
+++ src/components/Map/map.jsx
@@ -9,8 +9,9 @@ import Box from "@mui/material/Box";
 export default function Map() {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const center = { lng: 139.753, lat: 35.6844 };
-  const [zoom] = useState(14);
+  const geodata = configData.MAPTILER_DATSET_ID;
+  const center = { lng: -157.9253, lat: 21.4732 };
+  const [zoom] = useState(9.79); //9.79/21.4732/-157.9253
   maptilersdk.config.apiKey = configData.MAPTILER_API_KEY;
 
   useEffect(() => {
@@ -21,11 +22,27 @@ export default function Map() {
       style: maptilersdk.MapStyle.STREETS,
       center: [center.lng, center.lat],
       zoom: zoom,
+      hash: true,
+      terrain: true,
     });
 
-    new maptilersdk.Marker({ color: "#FF0000" })
-      .setLngLat([139.7525, 35.6846])
-      .addTo(map.current);
+    //Read more about MapTiler Point Helper: https://docs.maptiler.com/sdk-js/api/helpers/#point
+    map.current.on("load", async () => {
+      await maptilersdk.helpers.addPoint(map.current, {
+        data: geodata,
+        pointColor: maptilersdk.ColorRampCollection.COOL.scale(0, 30),
+        property: "minimum_nights",
+        pointOpacity: 0.5,
+        showLabel: true,
+        labelColor: "black",
+        // pointRadius: 10,
+        cluster: true,
+      });
+    });
+
+    // new maptilersdk.Marker({ color: "#FF0000" })
+    //   .setLngLat([139.7525, 35.6846])
+    //   .addTo(map.current);
   }, [center.lng, center.lat, zoom]);
 
   return (
