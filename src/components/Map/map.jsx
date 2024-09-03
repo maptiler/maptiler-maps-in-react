d8835018643c8a529e69588188fcda5c352be901
MagdalenaKuch <57178283+MagdalenaKuch@users.noreply.github.com>
2024-09-03 14:32:51 +0200

    E1 - clean up

--- src/components/Map/map.jsx
+++ src/components/Map/map.jsx
@@ -1,34 +1,33 @@
-import React, { useRef, useEffect, useState } from "react";
 import * as maptilersdk from "@maptiler/sdk";
 import "@maptiler/sdk/dist/maptiler-sdk.css";
-import "./map.css";
+import Box from "@mui/material/Box";
+import React, { useEffect, useRef, useState } from "react";
 import configData from "../../config";
 import Navbar from "../Navbar/navbar";
-import Box from "@mui/material/Box";
+import "./map.css";
 
 export default function Map() {
   const mapContainer = useRef(null);
   const map = useRef(null);
   const geodata = configData.MAPTILER_DATSET_ID;
   const center = { lng: -157.9253, lat: 21.4732 };
-  const [zoom] = useState(9.79); //9.79/21.4732/-157.9253
+  const zoom = 9.79; // 9.79/21.4732/-157.9253
   maptilersdk.config.apiKey = configData.MAPTILER_API_KEY;
 
   useEffect(() => {
     if (map.current) return; // stops map from intializing more than once
-
+    //map options: https://docs.maptiler.com/sdk-js/api/map/
     map.current = new maptilersdk.Map({
       container: mapContainer.current,
-      style: maptilersdk.MapStyle.STREETS,
+      style: maptilersdk.MapStyle.BACKDROP, //more about map styles: https://docs.maptiler.com/sdk-js/api/map-styles/
       center: [center.lng, center.lat],
       zoom: zoom,
       hash: true,
-      terrain: true,
     });
 
     //Read more about MapTiler Point Helper: https://docs.maptiler.com/sdk-js/api/helpers/#point
-    map.current.on("load", async () => {
-      await maptilersdk.helpers.addPoint(map.current, {
+    map.current.on("load", () => {
+      maptilersdk.helpers.addPoint(map.current, {
         data: geodata,
         pointColor: maptilersdk.ColorRampCollection.COOL.scale(0, 30),
         property: "minimum_nights",
@@ -36,17 +35,14 @@ export default function Map() {
         showLabel: true,
         labelColor: "black",
         // pointRadius: 10,
-        cluster: true,
+        // cluster: true,
       });
     });
-
-    // new maptilersdk.Marker({ color: "#FF0000" })
-    //   .setLngLat([139.7525, 35.6846])
-    //   .addTo(map.current);
   }, [center.lng, center.lat, zoom]);
 
   return (
     <Box sx={{ display: "flex" }}>
+      {/* Why is Sidebar and navbar in the Map? Watch E3 for explanation */}
       <Navbar />
       <div className="container">
         <div ref={mapContainer} id="map" className="map" />
