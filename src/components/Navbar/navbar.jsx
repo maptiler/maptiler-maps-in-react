4e46ad148e1d4ab29f4bb13876dcd24ada45806b
MagdalenaKuch <57178283+MagdalenaKuch@users.noreply.github.com>
2024-09-05 08:31:32 +0200

    change title in navbar

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -39,7 +39,7 @@ export default function Navbar({ handleDrawerOpen, open }) {
           <MenuIcon /> {/* ready for E3 where we are going to add a sidebar */}
         </IconButton>
         <Typography variant="h6" color="inherit" component="div">
-          Honolulu Accommodation
+          My React Map
         </Typography>
       </Toolbar>
     </AppBar>
