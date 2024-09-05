bb5e6c24932802938e51fc275fe618f7d09f9b37
MagdalenaKuch <57178283+MagdalenaKuch@users.noreply.github.com>
2024-09-05 15:00:17 +0200

    E1 - update readme

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   // Replace with YOUR_MAPTILER_API_KEY - this key is restricted to MapTiler Documentation pages.
   MAPTILER_API_KEY: "qC6zBtR7VUaKtyyiLcJq",
-  MAPTILER_API_KEY: "YOUR_MAPTILER_API_KEY",
-  MAPTILER_DATSET_ID: "YOUR_MAPTILER_DATSET_ID",
+  // Replace with YOUR_DATSET_ID
+  MAPTILER_DATSET_ID: "57ce7daa-ea5b-4cd6-8975-32c41f5c5d90",
 };
