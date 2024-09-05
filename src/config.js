2894cd695240ea3ea66d1f40d255d275f898e438
MagdalenaKuch <57178283+MagdalenaKuch@users.noreply.github.com>
2024-09-05 14:09:20 +0200

    Add restricted key

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export default {
-  MAPTILER_API_KEY: "YOUR_MAPTILER_API_KEY_HERE",
+  // Replace with YOUR_MAPTILER_API_KEY - this key is restricted to MapTiler Documentation pages.
+  MAPTILER_API_KEY: "qC6zBtR7VUaKtyyiLcJq",
 };
