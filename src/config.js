d83a63d419cecc8256d8d6884589235054a1d842
MagdalenaKuch <57178283+MagdalenaKuch@users.noreply.github.com>
2024-09-05 14:09:20 +0200

    Add restricted key

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
+  // Replace with YOUR_MAPTILER_API_KEY - this key is restricted to MapTiler Documentation pages.
+  MAPTILER_API_KEY: "qC6zBtR7VUaKtyyiLcJq",
   MAPTILER_API_KEY: "YOUR_MAPTILER_API_KEY",
   MAPTILER_DATSET_ID: "YOUR_MAPTILER_DATSET_ID",
 };
