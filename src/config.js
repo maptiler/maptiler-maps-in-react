7825a341ed593a659e44374d9ddc7482331c8a89
petr-hajek <petr.hajek@maptiler.com>
2024-09-19 14:35:53 +0200

    fix typo

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   // Replace with YOUR MAPTILER API KEY
   MAPTILER_API_KEY: import.meta.env.VITE_MAPTILER_API_KEY,
-  // Replace with YOUR_DATSET_ID
-  MAPTILER_DATSET_ID: "57ce7daa-ea5b-4cd6-8975-32c41f5c5d90",
+  // Replace with YOUR DATASET ID
+  MAPTILER_DATASET_ID: "57ce7daa-ea5b-4cd6-8975-32c41f5c5d90",
 };
